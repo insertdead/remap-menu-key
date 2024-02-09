58d0cb5fe26311d7ef56faae80d7afb9a30d1d31
insertdead <mdsc0111@tuta.io>
2024-02-09 18:04:06 -0500

    Add check to make escape key work properly as the menu key

--- src/main/java/mixin/client/KeyboardIgnoreEscapeKeyMixin.java
+++ src/main/java/mixin/client/KeyboardIgnoreEscapeKeyMixin.java
@@ -17,7 +17,10 @@
  */
 package mixin.client;
 
+import io.github.insertdead.remap_menu_key.client.RemapMenuKeyClient;
 import net.minecraft.client.Keyboard;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
 import org.lwjgl.glfw.GLFW;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -28,7 +31,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 abstract public class KeyboardIgnoreEscapeKeyMixin {
     @Inject(method = "onKey(JIIII)V", cancellable = true, at = @At("HEAD"))
     public void onKey(long window, int key, int scancode, int action, int modifiers, CallbackInfo ci) {
-        if (key == GLFW.GLFW_KEY_ESCAPE) {
+        if (key == GLFW.GLFW_KEY_ESCAPE && !RemapMenuKeyClient.menuBinding.matchesKey(key, scancode)) {
             ci.cancel();
         }
     }
