d90fedc68f68a9f38d125e087834cf33b4310176
insertdead <mdsc0111@tuta.io>
2024-02-09 17:36:33 -0500

    Format code

--- src/main/java/io/github/insertdead/remap_menu_key/client/RemapMenuKeyClient.java
+++ src/main/java/io/github/insertdead/remap_menu_key/client/RemapMenuKeyClient.java
@@ -1,20 +1,20 @@
 /*
-*    Remap Menu	Key, a Minecraft Mod to remap the menu (escape) key.
-*    Copyright (C) 2024 insertdead
-*
-*    This program is free software: you can redistribute it and/or modify
-*    it under the terms of the GNU General Public License as published by
-*    the Free Software Foundation, either version 3 of the License, or
-*    (at your option) any later version.
-*
-*    This program is distributed in the hope that it will be useful,
-*    but WITHOUT ANY WARRANTY; without even the implied warranty of
-*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*    GNU General Public License for more details.
-*
-*    You should have received a copy of the GNU General Public License
-*    along with this program.  If not, see <https://www.gnu.org/licenses/>.
-*/
+ *    Remap Menu	Key, a Minecraft Mod to remap the menu (escape) key.
+ *    Copyright (C) 2024 insertdead
+ *
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 package io.github.insertdead.remap_menu_key.client;
 
 import net.fabricmc.api.ClientModInitializer;
@@ -25,20 +25,20 @@ import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
 
 public class RemapMenuKeyClient implements ClientModInitializer {
-    public static KeyBinding menuBinding = KeyBindingHelper.registerKeyBinding(new KeyBinding(
-            "key.remap_menu_key.escape",
-            InputUtil.Type.KEYSYM,
-            InputUtil.GLFW_KEY_ESCAPE,
-            KeyBinding.MISC_CATEGORY
-    ));
+	public static KeyBinding menuBinding = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+		"key.remap_menu_key.escape",
+		InputUtil.Type.KEYSYM,
+		InputUtil.GLFW_KEY_ESCAPE,
+		KeyBinding.MISC_CATEGORY
+	));
 
-    @Override
-    public void onInitializeClient() {
-        ClientTickEvents.END_CLIENT_TICK.register(client -> {
-            if (menuBinding.wasPressed() && client.player != null) {
-                boolean bl3 = InputUtil.isKeyPressed(MinecraftClient.getInstance().getWindow().getHandle(), InputUtil.GLFW_KEY_F3);
-                client.openGameMenu(bl3);
-            }
-        });
-    }
+	@Override
+	public void onInitializeClient() {
+		ClientTickEvents.END_CLIENT_TICK.register(client -> {
+			if (menuBinding.wasPressed() && client.player != null) {
+				boolean bl3 = InputUtil.isKeyPressed(MinecraftClient.getInstance().getWindow().getHandle(), InputUtil.GLFW_KEY_F3);
+				client.openGameMenu(bl3);
+			}
+		});
+	}
 }
--- src/main/java/mixin/client/MenuRemapMixin.java
+++ src/main/java/mixin/client/MenuRemapMixin.java
@@ -19,11 +19,7 @@ package mixin.client;
 
 import io.github.insertdead.remap_menu_key.client.RemapMenuKeyClient;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.gui.screen.GameMenuScreen;
 import net.minecraft.client.gui.screen.Screen;
-import net.minecraft.client.gui.screen.ingame.HandledScreen;
-import net.minecraft.client.util.InputUtil;
-import net.minecraft.text.Text;
 import org.lwjgl.glfw.GLFW;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -34,25 +30,28 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 // Adds
 @Mixin(Screen.class)
 abstract public class MenuRemapMixin {
-    @Inject(method = "keyPressed(III)Z", at = @At("HEAD"), cancellable = true)
-    void onKeyPressed(int keyCode, int scanCode, int modifiers, CallbackInfoReturnable<Boolean> ci) {
-        MinecraftClient client = MinecraftClient.getInstance();
-        if (RemapMenuKeyClient.menuBinding.matchesKey(keyCode, scanCode)) {
-            if (client.currentScreen == null) {
-                client.openGameMenu(false);
-            } else if (this.shouldCloseOnEsc()) {
-                this.close();
-            }
+	@Inject(method = "keyPressed(III)Z", at = @At("HEAD"), cancellable = true)
+	void onKeyPressed(int keyCode, int scanCode, int modifiers, CallbackInfoReturnable<Boolean> ci) {
+		MinecraftClient client = MinecraftClient.getInstance();
+		if (RemapMenuKeyClient.menuBinding.matchesKey(keyCode, scanCode)) {
+			if (client.currentScreen == null) {
+				client.openGameMenu(false);
+			} else if (this.shouldCloseOnEsc()) {
+				this.close();
+			}
 
-            ci.setReturnValue(true);
-        } else if (keyCode == GLFW.GLFW_KEY_ESCAPE) {
-            ci.setReturnValue(false);
-        }
-    }
+			ci.setReturnValue(true);
+		} else if (keyCode == GLFW.GLFW_KEY_ESCAPE) {
+			ci.setReturnValue(false);
+		}
+	}
 
-    @Shadow
-    public void close() {}
+	@Shadow
+	public void close() {
+	}
 
-    @Shadow
-    public boolean shouldCloseOnEsc() {return true;}
+	@Shadow
+	public boolean shouldCloseOnEsc() {
+		return true;
+	}
 }
